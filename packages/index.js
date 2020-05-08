5768ebf8b76002a8ea5b4ebeaf641db8eb8c272a
半人类~ <37428009+Finyu@users.noreply.github.com>
2020-05-08 19:20:29 +0800

    修改了一些配置和index.Md的内容

--- packages/index.js
+++ packages/index.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+
 import zTree from './zTree/index.js'
 import zButton from './zButton/index.js'
 import zMonth from './zMonth/index.js'
@@ -9,19 +9,20 @@ import zRing from './zRing/index.js'
 const components =  [zTree, zMonth, zButton, zPiechart, zRing]
 
 const install = function(Vue, opts = {}) {
+  Vue.use(fullScreen)
   components.forEach(component => {
     Vue.component(component.name, component)
   })
 }
 
-Vue.use(fullScreen)
 /* istanbul ignore if */
-if (typeof window !== 'undefined' ) {
-  install(Vue)
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
 }
 
 export default {
   version: '1.0.0',
+  install,
   zTree,
   zMonth,
   zButton,
